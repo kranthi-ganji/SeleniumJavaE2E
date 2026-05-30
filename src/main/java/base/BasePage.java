2ccb47c96e5e556c63e60a732a754eb308eefedf
Your Name <you@example.com>
2026-05-30 18:12:56 +0530

    Setup of Selenium Java Framewokr

---
+++ src/main/java/base/BasePage.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+	 WebDriver driver;
+	
+	public BasePage(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void click(WebElement element) {
+		element.click();
+	}
+	
+	public String getText(WebElement element) {
+		return element.getText();
+	}
+	
+	public void type(WebElement element,String text) {
+		element.sendKeys(text);
+	}
+}
---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import base.BasePage;
+
+public class LoginPage extends BasePage{
+	WebDriver driver;
+
+	By userName = By.xpath("//input[@name='username']");
+	By password = By.xpath("//input[@name='password']");
+	By loginBtn = By.xpath("//button[@type='submit']");
+	
+	public LoginPage(WebDriver driver){
+		super(driver);
+		this.driver = driver;
+	}
+	
+	public void enterUsername(String name) {
+		type(driver.findElement(userName),name);
+	}
+	
+	public void enterPassword(String pwd) {
+		type(driver.findElement(password),pwd);
+	}
+	
+	public void clickLoginBtn() {
+		click(driver.findElement(loginBtn));
+	}
+	
+}
